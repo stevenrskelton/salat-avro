f06a873ae0598f9decea24be6374948f9b036013
Julian Peeters <julianpeeters@gmail.com>
2012-08-30 00:07:52 -0700

    read to and from in-memory stream, write to a file, but cannot read in an avro from an avrofile

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -4,14 +4,14 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with ApacheLicense
   val novusRels = "repo.novus rels" at "http://repo.novus.com/releases/"
   val novusSnaps = "repo.novus snaps" at "http://repo.novus.com/snapshots/"
 
+  val scalatoolsRels = "repo.scalatools rels" at "https://oss.sonatype.org/content/groups/scala-tools/"
+  val scalatoolsSnaps = "repo.scalatools snaps" at "https://oss.sonatype.org/content/repositories/snapshots/"
+
   val salat = "com.novus" % "salat-core_2.9.0-1" % "0.0.8-SNAPSHOT"
-  val avro = "org.apache.avro" % "avro" % "1.5.1"
+  val avro = "org.apache.avro" % "avro" % "1.7.1"
 
   // ScalaSig in 2.9.1 depends on the scala-compiler - OUCH!
   val scalaCompiler = "org.scala-lang" % "scala-compiler" % "2.9.1"
-
-  // necessary for scalaz snapshot that specs2 depends on
-  val snapshots = "snapshots" at "http://scala-tools.org/repo-snapshots"
 
   val specs2 = "org.specs2" %% "specs2" % "1.8.1" % "test"
   def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
--- src/main/scala/AvroGrater.scala
+++ src/main/scala/AvroGrater.scala
@@ -17,8 +17,12 @@ package com.banno.salat.avro
 
 import com.novus.salat._
 import org.apache.avro.Schema
-import org.apache.avro.io.{ Decoder, Encoder, DatumReader, DatumWriter }
+import org.apache.avro.io._
+import org.apache.avro.file._
+import org.apache.avro.specific._
+import org.apache.avro.reflect._
 import scala.collection.mutable.ListBuffer
+import java.io._
 
 trait AvroGrater[X <: AnyRef] {
   implicit val ctx: Context
@@ -26,6 +30,14 @@ trait AvroGrater[X <: AnyRef] {
   private[avro] def asSingleAvroSchema(knownSchemas: ListBuffer[Schema]): Schema
   def +(other: AvroGrater[_]): MultiAvroGrater
   def supports[X](x: X)(implicit manifest: Manifest[X]): Boolean
+
+  lazy val asDatumWriter: DatumWriter[X] = new AvroGenericDatumWriter[X](asAvroSchema)
+  lazy val asDatumReader: AvroDatumReader[X] = asGenericDatumReader
+  lazy val asGenericDatumReader: AvroGenericDatumReader[X] = new AvroGenericDatumReader[X](asAvroSchema)
+
+//------------------------------IN-MEMORY------------------------------------------
+//Serialize to an in-memory stream with serialize,
+//Serialize from in in-memory stream to an object with asObject
 
   def serialize(x: X, encoder: Encoder): Encoder = try {
     asDatumWriter.write(x, encoder)
@@ -37,8 +49,27 @@ trait AvroGrater[X <: AnyRef] {
 
   def asObject(decoder: Decoder): X = asDatumReader.read(decoder)
 
-  lazy val asDatumWriter: DatumWriter[X] = new AvroGenericDatumWriter[X](asAvroSchema)
-  lazy val asDatumReader: AvroDatumReader[X] = asGenericDatumReader
-  lazy val asGenericDatumReader: AvroGenericDatumReader[X] = new AvroGenericDatumReader[X](asAvroSchema)
 
+//-------------------------------TO/FROM AVRO DATAFILE----------------------------------------------------------------
+//Serialize to an Avro file with serializeToDataFile, needs to be passed a schema, file, datum, and DataFileWriter
+//deserialize from file with asObjectFromDataFile
+
+//Writing to File
+  lazy val asDataFileWriter: DataFileWriter[X] = new DataFileWriter[X](asDatumWriter)
+
+  def serializeToDataFile(schema: Schema, outfile: File, x: X): DataFileWriter[X] = try {
+    asDataFileWriter.create(schema, outfile)  
+    asDataFileWriter.append(x)          //while datafilewriter.hasNext() {asDataFileWriter.append(x)}??????
+    asDataFileWriter.close
+    asDataFileWriter
+  } catch {
+    case e: Throwable => throw new AvroSerializationException(this, x, e)
+  }
+
+//Reading from File  
+
+  def asObjectFromDataFile(infile: File): X = {
+    val asDataFileReader: DataFileReader[X] = new DataFileReader[X](infile, asDatumReader)
+    asDataFileReader.next()
+  }  
 }
